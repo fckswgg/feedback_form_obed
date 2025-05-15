e52d85302226603fa82749f9cc33729d63eb41f3
fckswgg <136038685+fckswgg@users.noreply.github.com>
2025-05-15 04:09:53 +0300

    Update feedback.js

--- feedback.js
+++ feedback.js
@@ -1,33 +1,35 @@
-const form = document.querySelector('form');
-console.log(document.querySelector('form'));
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.getElementById('#feedback-form');
 
-form.addEventListener('submit', async (e) => {
-  e.preventDefault();
-
-  const data = {
-    name: form.querySelector('input[name="name"]').value.trim(),
-    phone: form.querySelector('input[name="phone"]').value.trim(),
-    rating: form.querySelector('input[name="rating"]').value || '',
-    comment: form.querySelector('textarea[name="comment"]').value.trim() || '',
-  };
-
-  // Здесь можно добавить проверку обязательных полей
-
-  try {
-    const response = await fetch('https://script.google.com/macros/s/AKfycbyGHSXBuqjpyW-AC1zOQENLMtLjzhtU-4pnCrTaEkqSDi7fPn0Z71FVR10jOGyIgmk/exec', {
-      method: 'POST',
-      body: JSON.stringify(data),
-      headers: {'Content-Type': 'application/json'}
-    });
-    const result = await response.json();
-
-    if (result.result === 'success') {
-      alert('Спасибо за отзыв!');
-      form.reset();
-    } else {
-      alert('Ошибка при отправке: ' + result.error);
+  console.log(document.querySelector('#feedback-form'));
+  
+  form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+  
+    const data = {
+      name: form.querySelector('input[name="name"]').value.trim(),
+      phone: form.querySelector('input[name="phone"]').value.trim(),
+      rating: form.querySelector('input[name="rating"]').value || '',
+      comment: form.querySelector('textarea[name="comment"]').value.trim() || '',
+    };
+  
+    // Здесь можно добавить проверку обязательных полей
+  
+    try {
+      const response = await fetch('https://script.google.com/macros/s/AKfycbyGHSXBuqjpyW-AC1zOQENLMtLjzhtU-4pnCrTaEkqSDi7fPn0Z71FVR10jOGyIgmk/exec', {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {'Content-Type': 'application/json'}
+      });
+      const result = await response.json();
+  
+      if (result.result === 'success') {
+        alert('Спасибо за отзыв!');
+        form.reset();
+      } else {
+        alert('Ошибка при отправке: ' + result.error);
+      }
+    } catch (error) {
+      alert('Ошибка сети: ' + error.message);
     }
-  } catch (error) {
-    alert('Ошибка сети: ' + error.message);
-  }
-});
+  });
